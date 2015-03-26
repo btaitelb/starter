3bc4843800430323ff23251ac40a9099890a25e9
Ben Taitelbaum <ben@coshx.com>
2015-03-26 19:17:22 -0400

    starts to add prototype for tracking foods

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :users
   root 'home#index'
 
+  # this isn't the best way to make routes,
+  # but is fine for today (prototyping)
+  get '/track', to: 'home#track'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
